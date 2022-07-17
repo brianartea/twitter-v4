bc1ca25af34f84f964d96f9b62324f44a8e67b5a
brianartea <bri@expertdesign.cc>
2022-07-17 11:06:51 -0400

    commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,8 +5,8 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
@@ -15,7 +15,7 @@ export default NextAuth({
     signIn: "/auth/signin",
   },
 
-  secret: process.env.SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
 
   callbacks: {
     async session({ session, token }) {
